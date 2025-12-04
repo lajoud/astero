56167f49cfda8391351c8c17c6bc2de237f8829d
lajoud <nicolas.joudiou@gmail.com>
2025-12-04 15:44:40 +0100

    adding player class and rotation control for the player

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
+    my_clock=pygame.time.Clock()
+    dt=0
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    new_player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
 
     while True:
         log_state()
@@ -14,7 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        new_player.update(dt)
+        new_player.draw(screen)
         pygame.display.flip()
+        
+        dt =my_clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
